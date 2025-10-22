5c995ee60a4cb03a0b61694ccfb452da90998411
[George Kurniawan] <georgeadrielk@gmail.com>
2025-10-22 19:55:16 +0700

    1.0

---
+++ main.py
@@ -0,0 +1,16 @@
+from collections import Counter
+import re
+
+def get_frequency(text: str) -> list[tuple[str, int]]:
+    lowered_text: str = text.lower()
+    words: list[str] = re.findall(r'\b\w+\b', lowered_text)
+    word_counts: Counter = Counter(words)
+    return word_counts.most_common()
+
+def main() -> None:
+    text: str = input('Enter your text: ').strip()
+    word_frequencies: list[tuple[str, int]] = get_frequency(text)
+    print(word_frequencies)
+
+if __name__ == '__main__':
+    main()
